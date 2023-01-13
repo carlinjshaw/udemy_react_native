20562404e9160757a8c0154c5dfdc0052e43d975
carlinjshaw <carlinjshaw@gmail.com>
2023-01-12 22:20:49 -0500

    used react natives createStackNavigator to access multiple pages

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>yup i do</Text>;
+const HomeScreen = ({navigation}) => {
+
+  return <View>
+    <Text style={styles.text}>yup i do</Text>
+    <Button 
+    onPress={() => navigation.navigate('Components')}
+    title = "Go to components demo"
+    />
+    <Button 
+    onPress={() => navigation.navigate('List')}
+    title = "Go to List Demo"
+    />
+    </View>
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,27 +4,35 @@ import { FlatList } from 'react-native-gesture-handler';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1'},
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'},
-        {name: 'Friend #10'}
+        {name: 'Friend #1', age: 23},
+        {name: 'Friend #2', age: 27},
+        {name: 'Friend #3', age: 28},
+        {name: 'Friend #4', age: 30},
+        {name: 'Friend #5', age: 32},
+        {name: 'Friend #6', age: 33},
+        {name: 'Friend #7', age: 27},
+        {name: 'Friend #8', age: 25},
+        {name: 'Friend #9', age: 34}
     ]
 
     return <FlatList 
+    
+    showsHorizontalScrollIndicator= {false}
+        keyExtractor={friend => friend.name}
         data = {friends} 
         renderItem= {({item}) => {
-            return <Text>{item.name}</Text>
+            return <View>
+            <Text style = {styles.textStyle}>{item.name}  Age: {item.age} </Text>
+            {/* <Text>       Age: {item.age} </Text> */}
+            </View>
     }}
     />
 }
 
 const styles = StyleSheet.create({
-
+    textStyle: {
+        marginVertical: 50
+    }
 })
 
 export default ListScreen;
